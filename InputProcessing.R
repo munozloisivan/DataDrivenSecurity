be86c6db63b15f782bde260d4236c98a3ec245ae
munozloisivan <munozloisivan@gmail.com>
2019-12-17 19:11:37 +0100

    Input & Processing files creation

---
+++ InputProcessing.R
@@ -0,0 +1,4 @@
+library(readr)
+inputReportDf <- read_csv("C:/Users/ivanm/Desktop/Master/5. Data Driven Security/Scan_Results_abtes2vm_20191217_scan_1574870445_41138.csv",
+skip = 6)
+View(inputReportDf)
---
+++ KnowledgeBase.R
@@ -0,0 +1,5 @@
+library(readr)
+
+KnowledgeBase <- read_csv("C:/Users/ivanm/Desktop/Master/5. Data Driven Security/DL_vulnerabilities_abtes2vm_20191217.csv", skip = 3)
+
+view(KnowledgeBase)
