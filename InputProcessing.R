28ce0c3a465dfa6f0bb75a06ca8b07a6d919a219
munozloisivan <munozloisivan@gmail.com>
2020-01-02 12:17:16 +0100

    Filtering data to be processed

--- InputProcessing.R
+++ InputProcessing.R
@@ -2,3 +2,11 @@ library(readr)
 inputReportDf <- read_csv("C:/Users/ivanm/Desktop/Master/5. Data Driven Security/Scan_Results_abtes2vm_20191217_scan_1574870445_41138.csv",
 skip = 6)
 View(inputReportDf)
+
+#Filtramos el Report que extraemos para quedarnos únicamente
+#con aquellas columnas que nos interesan
+mydf <- inputReportDf[,c("IP", "QID", "Type", "Severity", "CVE ID")]
+View(mydf)
+
+#IPdf <- df[,c("IP","QID")]
+#View(IPdf)
--- KnowledgeBase.R
+++ KnowledgeBase.R
@@ -1,5 +1,8 @@
 library(readr)
 
-KnowledgeBase <- read_csv("C:/Users/ivanm/Desktop/Master/5. Data Driven Security/DL_vulnerabilities_abtes2vm_20191217.csv", skip = 3)
+KnowledgeBaseDf <- read_csv("C:/Users/ivanm/Desktop/Master/5. Data Driven Security/DL_vulnerabilities_abtes2vm_20191217.csv", skip = 3)
+view(KnowledgeBaseDf)
 
-view(KnowledgeBase)
+#Filtramos la KDB para quedarnos únicamente con aquellas columnas que nos pueden interesar
+KDB_df <- KnowledgeBaseDf[,c("QID", "Category", "Modified", "Published", "CVE ID")]
+View(KDB_df)
